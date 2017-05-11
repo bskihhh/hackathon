f7ded4602ddb2fdf80cb8364d68154f5efae95f4
Bo Chen <Bo.Chen@intelliware.ca>
2017-05-11 15:59:06 -0400

    qr scanner

--- android/app/src/main/java/com/getgobo/gobopay/MainActivity.java
+++ android/app/src/main/java/com/getgobo/gobopay/MainActivity.java
@@ -1,13 +1,26 @@
 package com.getgobo.gobopay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button registerTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.registerTable = (Button) findViewById(R.id.register_table);
+        this.registerTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ScannerActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
